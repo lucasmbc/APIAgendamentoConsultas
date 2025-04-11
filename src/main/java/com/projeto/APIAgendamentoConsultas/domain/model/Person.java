ed02877e7b3e5a5347bca736745cce8f9d4f341f
lucasmbc <lucasmbc@hotmail.com>
2025-04-11 18:20:05 -0300

    feat: add abstract class person

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/domain/model/Person.java
@@ -0,0 +1,28 @@
+package com.projeto.APIAgendamentoConsultas.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
