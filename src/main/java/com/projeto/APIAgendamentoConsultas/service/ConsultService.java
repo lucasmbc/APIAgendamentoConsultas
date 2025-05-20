8dd91287bc9d0e874665affa29c0a99a55a98322
lucasmbc <lucasmbc@hotmail.com>
2025-05-20 17:39:11 -0300

    feat: add interface ConsultService

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/service/ConsultService.java
@@ -0,0 +1,6 @@
+package com.projeto.APIAgendamentoConsultas.service;
+
+import com.projeto.APIAgendamentoConsultas.domain.model.Consult;
+
+public interface ConsultService extends CrudService<Long, Consult> {
+}
