79a4376a3fd04988113e54b7a0af8a651c6dffed
lucasmbc <lucasmbc@hotmail.com>
2025-05-20 17:50:35 -0300

    feat: add interface PatientService

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/service/PatientService.java
@@ -0,0 +1,6 @@
+package com.projeto.APIAgendamentoConsultas.service;
+
+import com.projeto.APIAgendamentoConsultas.domain.model.Patient;
+
+public interface PatientService extends CrudService<Long, Patient>{
+}
