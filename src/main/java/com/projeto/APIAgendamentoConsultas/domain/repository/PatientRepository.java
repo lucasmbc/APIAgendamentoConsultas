5a6ac94a4690145b49b2134b8f0c9d916113fe00
lucasmbc <lucasmbc@hotmail.com>
2025-05-20 17:37:23 -0300

    feat: add interface PatientRepository

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/domain/repository/PatientRepository.java
@@ -0,0 +1,26 @@
+package com.projeto.APIAgendamentoConsultas.domain.repository;
+
+import com.projeto.APIAgendamentoConsultas.domain.model.Patient;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
+
+//    @Query("SELECT DISTINCT p FROM Patient p LEFT JOIN FETCH p.consultations")
+    @EntityGraph(attributePaths = "consultations")
+    @Query("SELECT p FROM Patient p")
+    List<Patient> findAllWithConsultations();
+
+    @EntityGraph(attributePaths = "consultations")
+    @Query("SELECT p FROM Patient p WHERE p.id = :id")
+    Optional<Patient> findByIdWithConsultations(@Param("id") Long id);
+}
