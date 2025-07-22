83c2f96d88ce38028b0f0b9cb386cb6bd97a92d7
lucasmbc <lucasmbc@hotmail.com>
2025-07-22 16:42:25 -0300

    feat: add JwtCustomAuthenticationFilter

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/security/JwtCustomAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.projeto.APIAgendamentoConsultas.security;
+
+import com.projeto.APIAgendamentoConsultas.domain.model.User;
+import com.projeto.APIAgendamentoConsultas.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtCustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(mustConvert(authentication)) {
+            String userName = authentication.getName();
+            User user = userService.findByUsername(userName);
+            if(user != null) {
+                authentication = new CustomAuthentication(user);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean mustConvert(Authentication authentication) {
+        return authentication != null && authentication instanceof JwtAuthenticationToken;
+    }
+}
