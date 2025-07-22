8bd3facefeac0fd635bad7cbf5d6ce3a32cb602f
lucasmbc <lucasmbc@hotmail.com>
2025-07-22 16:42:11 -0300

    feat: add CustomUserDetailsService

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.projeto.APIAgendamentoConsultas.security;
+
+import com.projeto.APIAgendamentoConsultas.domain.model.User;
+import com.projeto.APIAgendamentoConsultas.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[user.getRoles().size()]))
+                .build();
+    }
+}
