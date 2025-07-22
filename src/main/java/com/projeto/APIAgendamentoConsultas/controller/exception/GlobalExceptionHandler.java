468186267c2a2840078b4fe1bc406b5f13b13f65
lucasmbc <lucasmbc@hotmail.com>
2025-07-22 17:42:37 -0300

    feat: import UsernameNotFoundException

--- src/main/java/com/projeto/APIAgendamentoConsultas/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/projeto/APIAgendamentoConsultas/controller/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
