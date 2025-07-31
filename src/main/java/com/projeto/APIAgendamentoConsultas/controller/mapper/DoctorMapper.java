119522b53e56a61893a0106947cfe0f531a49037
lucasmbc <lucasmbc@hotmail.com>
2025-07-31 17:29:00 -0300

    feat: add DoctorMapper

---
+++ src/main/java/com/projeto/APIAgendamentoConsultas/controller/mapper/DoctorMapper.java
@@ -0,0 +1,38 @@
+package com.projeto.APIAgendamentoConsultas.controller.mapper;
+
+import com.projeto.APIAgendamentoConsultas.controller.dto.ConsultDto;
+import com.projeto.APIAgendamentoConsultas.controller.dto.DoctorRequestDto;
+import com.projeto.APIAgendamentoConsultas.controller.dto.DoctorResponseDto;
+import com.projeto.APIAgendamentoConsultas.domain.model.Doctor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+
+@Component
+@RequiredArgsConstructor
+public class DoctorMapper {
+
+    public Doctor toModel(DoctorRequestDto dto) {
+        Doctor doctor = new Doctor();
+        doctor.setName(dto.name());
+        doctor.setEmail(dto.email());
+        doctor.setPhone(dto.phone());
+        doctor.setCrm(dto.crm());
+        doctor.setSpecialty(dto.specialty());
+        return doctor;
+    }
+
+    public DoctorResponseDto toResponseDto(Doctor doctor) {
+        return new DoctorResponseDto(
+                doctor.getId(),
+                doctor.getName(),
+                doctor.getEmail(),
+                doctor.getPhone(),
+                doctor.getCrm(),
+                doctor.getSpecialty(),
+                ofNullable(doctor.getConsultations()).orElse(emptyList()).stream().map(ConsultDto::new).toList()
+        );
+    }
+}
