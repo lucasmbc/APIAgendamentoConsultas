7b727437f22df423d5466a5717a6df95b3797d26
lucasmbc <lucasmbc@hotmail.com>
2025-08-03 16:05:44 -0300

    feat: Add findByName and findCrm methods to DoctorService

--- src/main/java/com/projeto/APIAgendamentoConsultas/service/DoctorService.java
+++ src/main/java/com/projeto/APIAgendamentoConsultas/service/DoctorService.java
@@ -3,8 +3,11 @@ package com.projeto.APIAgendamentoConsultas.service;
 import com.projeto.APIAgendamentoConsultas.domain.model.Doctor;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DoctorService extends CrudService<UUID, Doctor>{
     List<Doctor> findBySpecialty(String specialty);
+    List<Doctor> findByName(String name);
+    List<Doctor> findCrm(String crm);
 }
