37df5c71ab5021e07e1894ca8fc1ab6d31fffd08
lucasmbc <lucasmbc@hotmail.com>
2025-08-04 21:48:52 -0300

    feat: Add findByName and findByCpf methods to PatientService

--- src/main/java/com/projeto/APIAgendamentoConsultas/service/PatientService.java
+++ src/main/java/com/projeto/APIAgendamentoConsultas/service/PatientService.java
@@ -2,7 +2,10 @@ package com.projeto.APIAgendamentoConsultas.service;
 
 import com.projeto.APIAgendamentoConsultas.domain.model.Patient;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PatientService extends CrudService<UUID, Patient>{
+    List<Patient> findByName(String name);
+    List<Patient> findByCpf(String cpf);
 }
