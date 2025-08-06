d7f505809132030e2401499fe4181ea6afe29d0e
lucasmbc <lucasmbc@hotmail.com>
2025-08-06 18:35:10 -0300

    feat: Add method findByDoctorIdAndStartDateBetween

--- src/main/java/com/projeto/APIAgendamentoConsultas/service/ConsultService.java
+++ src/main/java/com/projeto/APIAgendamentoConsultas/service/ConsultService.java
@@ -2,7 +2,10 @@ package com.projeto.APIAgendamentoConsultas.service;
 
 import com.projeto.APIAgendamentoConsultas.domain.model.Consult;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface ConsultService extends CrudService<UUID, Consult> {
+    List<Consult> findByDoctorIdAndStartDateBetween(UUID doctorId, LocalDateTime start, LocalDateTime end);
 }
